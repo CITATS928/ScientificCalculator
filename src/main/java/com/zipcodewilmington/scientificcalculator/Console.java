9ad0a3309a42bdd6c4dcb98b81c86a1f8b62da53
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-01 18:16:09 -0400

    Done of the Project

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -1,19 +1,17 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Random;
 import java.util.Scanner;
 
-/**
- * Created by leon on 2/9/18.
- */
-public class  Console {
-
+//create java class named console for all calculations
 public class Console {
-    //declare scanner
+    // create new scanner instance of the built in scanner class
     private static Scanner scanner = new Scanner(System.in);
-    //declare memory as 0 first value
+    //declare memory to store a value, initialize as 0
+    //us a double floating-point number for memory (numbers with decimal points)
     private static double memory = 0.0; // Memory variable to store result
-
-
+    public static Random randomGenerator = new Random(System.currentTimeMillis());
+    static int counter=0;
     //declare print (print output)
     public static void print(String output, Object... args) {
         System.out.printf(output, args);
@@ -26,7 +24,8 @@ public class Console {
 
     public static String getStringInput(String prompt) {
         println(prompt);
-        return scanner.nextLine();
+        String abc= scanner.nextLine();
+        return abc;
     }
 
     public static int getIntegerInput(String prompt) {
@@ -50,46 +49,43 @@ public class Console {
             }
         }
     }
-
+    // method to store value into memory
     public static void storeInMemory(double result) {
+        // check if memory IS NOT a non-number
         if (!Double.isNaN(result)) {
             memory = result;
-            println("Result stored in memory.");
+            println("Your result is stored to memory.");
         }
     }
-
+    // method to recall memory value
     public static double recallMemory() {
-        println("Recalling result from memory: " + memory);
+        //println("Recalling result from memory: " + memory);
         return memory;
     }
-    // de
+    // method to
     public static void clearMemory() {
         memory = 0.0;
-        println("Memory cleared.");
+        println(memory + " Memory cleared.");
     }
-    // declare operator variables in a constructor
     // takes num 1, an operator, and num 2 and returns result, after checking each case
     // the class name = performOperation
     public static double performOperation(double num1, char operator, double num2) {
-        //initialize display to 0
+        // declare + initialize display result to 0
         double result = 0.0;
-
+        // declare + initialize
+        //check if memory
         //define switch cases for operator
         switch (operator) {
             case '+':
-                result = add(num1,num2);
-//                result = num1 + num2; //addition
+                //result = add(num1,num2);
+                result = Console.add(num1,num2); //addition
                 break;
             case '-':
                 result = num1 - num2; //subtraction
                 break;
             case '*':
-                if (num1 == 0 || num2 == 0) { //return zero
-                    println("Multiplied by zero");
-                    result = 0; // Indicate error
-                } else {
-                    result = num1 * num2;
-                }
+                result = num1 * num2;
+
                 break;
             case '/':
                 if (num2 != 0)
@@ -106,10 +102,293 @@ public class Console {
                 result = Double.NaN; // Indicate error
                 break;
         }
-
         //call store in memory method to store result
         storeInMemory(result); // Store result in memory
 
         return result;
     }
+    // Qian
+    // trig functions
+    public static double TrigFunction(){
+        System.out.println("welcome to Trig Function: \n");
+        System.out.println("Choose from the following option: \n1.Sine\n2.Cosine\n3.Tangent\n4.Inverse Sine\n5.Inverse Cosine\n6.Inverse Tangent");
+
+        // consume newline char from buffer
+        //scanner.nextLine();
+
+        double result=0;
+        int input=scanner.nextInt();
+
+        switch (input){
+
+            case 1:
+                System.out.println("sin__");
+                double sininput = scanner.nextDouble();
+                result = Math.sin(sininput);
+                break;
+            case 2:
+                System.out.println("cos__");
+                double cosinput = scanner.nextDouble();
+                result=Math.cos(cosinput);
+                break;
+            case 3:
+                System.out.println("tan__");
+                double taninput = scanner.nextDouble();
+                result=Math.tan(taninput);
+                break;
+            case 4:
+                System.out.println("inverseSin___'");
+                double insin=scanner.nextDouble();
+                result =Math.asin(insin);
+                break;
+            case 5:
+                System.out.println("inverseCos___'");
+                double incos=scanner.nextDouble();
+                result = Math.acos(incos);
+                break;
+            case 6:
+                System.out.println("inverseTan___");
+                double intan = scanner.nextDouble();
+                result = Math.atan(intan);
+                break;
+            default:
+                System.out.println("invalid\nxxxxxxxxxxxxxxxxx");
+                //error();
+                break;
+
+        }
+        //
+        storeInMemory(result);
+
+        // consume newline char from buffer
+        Console.print("'In Radians' ");
+        scanner.nextLine();
+        //System.out.println("result is "+result);
+        return result;
+    }
+    //Jared
+    public static double absoluteValue(double num){
+        return Math.abs(num);
+    }
+    //Jared
+    // declare method for advanced functions
+    public static double AdvFunction(){
+        System.out.println("Advanced Functions: \n");
+        System.out.println("Choose from following options: \n 1. Squared \n 2. Square Root \n 3. Switch Sign \n 4. Variable Exponent \n 5. Factorial \n 6. Inverse");
+        double result = 0;
+        int input = scanner.nextInt();
+
+        // consume newline char from buffer
+        scanner.nextLine();
+
+        switch (input){
+            case 1:
+                System.out.println("number to be squared....");
+                double squareInp = scanner.nextDouble();
+                result = squareInp * squareInp;
+                break;
+            case 2:
+                System.out.println("find square root of.....");
+                double sqrtInp = scanner.nextDouble();
+                result = Math.sqrt(sqrtInp);
+                break;
+            case 3:
+                System.out.println("switch the sign of.....");
+                double sw = scanner.nextDouble();
+                result= -(sw);
+                break;
+            case 4:
+                System.out.println("Pick initial number.....");
+                double num1 = scanner.nextDouble();
+                System.out.println("Pick exponent number.....");
+                double num2 = scanner.nextDouble();
+                result= Math.pow(num1, num2);
+                break;
+            case 5:
+                double temp=1;
+                Console.println("Factorial number is ___");
+                double fac = scanner.nextDouble();
+                for(int i=1;i<=fac;i++){
+                    temp*=i;
+                }
+                result=temp;
+                //Console.println("your result is "+result);
+                break;
+            case 6:
+                Console.println("The number you want to inverse");
+                int invnum = scanner.nextInt();
+                //Console.println("1.0/"+invnum);
+                result = 1.0 / invnum;
+                break;
+            default:
+                System.out.println("invalid\nxxxxxxxxxxxxxxxxx");
+                //error();
+                break;
+        }
+        // consume newline char from buffer
+        scanner.nextLine();
+        storeInMemory(result);
+        //System.out.println("The result is " + result);
+        return result;
+    }
+    // Jared
+    // declare method for switching
+    public static String switchDisplay(String mode){
+        int number = (int) recallMemory();
+        String result = "";
+
+        // Create a series of boolean values that represent whether the mode equals "b", "o", "h", or "d"
+        // Ignoring case, so "B" and "b" are valid inputs
+        boolean isBinary = mode.equalsIgnoreCase("b");
+        boolean isOctal = mode.equalsIgnoreCase("o");
+        boolean isHex = mode.equalsIgnoreCase("h");
+        boolean isDecimal = mode.equalsIgnoreCase("d");
+
+        if (isBinary) {
+            result = Integer.toBinaryString(number);
+            Console.print("As Binary.." + result + '\n');
+        }
+        else if (isOctal) {
+            result = Integer.toOctalString(number);//number.toString(100, 8);
+            Console.print("As Octal.." + result + '\n');
+        }
+        else if (isHex) {
+            result = Integer.toHexString(number).toUpperCase();
+            Console.print("As Hexadecimal.." + result + '\n');
+        }
+        else if (isDecimal) {
+            result = Integer.toString(number);
+            Console.print("As Decimal.." + result + '\n');
+        }
+        else {
+            result = "Invalid";
+        }
+
+        return result;
+    }
+    //Jared
+    // switch units from radians to degrees
+    public static double switchUnitsMode(String mode){
+        double scientificMemory = recallMemory();
+
+        // Creating a series of boolean values that represent whether the input equals "r" or "d"
+        boolean isRadians = mode.equalsIgnoreCase("r");
+        boolean isDegrees = mode.equalsIgnoreCase("d");
+
+        if (isRadians) {
+            scientificMemory = Math.toRadians((scientificMemory));
+            Console.print("Radians is " + scientificMemory);
+        } else if (isDegrees) {
+            scientificMemory = Math.toDegrees(scientificMemory);
+            Console.print("Degree is " + scientificMemory);
+        }
+        else {
+            Console.println("Invalid");
+            scientificMemory = 0;
+        }
+
+        return scientificMemory;
+    }
+    //Jared
+    // custom function random
+    public static double random() {
+        double randomNumber = Math.random()*10000;
+        storeInMemory(Math.round(randomNumber));
+        return Math.round(randomNumber);
+    }
+    //Qian
+    // declare method for Log functions
+    public static double Logarithmic(){
+        System.out.println("Welcome to Logarithmic Functions:\n");
+        System.out.println("Choose from following options: \n1.Log(x) base10 \n2.Log(x) inverseLog base10 \n3.ln(x) \n4.ln(x) inverseLog" );
+        double result=0;
+        int input=scanner.nextInt();
+
+        // consume newline char from buffer
+        scanner.nextLine();
+
+        switch (input){
+            case 1:
+                System.out.println("Log:_");
+                double loginput=scanner.nextDouble();
+                result = Math.log10(loginput);
+                break;
+            case 2:
+                System.out.println("Inverse Log_");
+                double inverselog=scanner.nextDouble();
+                result = Math.pow(10, inverselog);
+                break;
+            case 3:
+                System.out.println("screen: ln_ ");
+                double lninput = scanner.nextDouble();
+                result= Math.log(lninput);
+                break;
+            case 4:
+                System.out.println("screen : inverse ln_");
+                double inverseln = scanner.nextDouble();
+                result=Math.exp(inverseln);
+                break;
+            default:
+                System.out.println("invalid\nxxxxxxxxxxxxxxxxx");
+                //error();
+                break;
+        }
+        // consume newline char from buffer
+        scanner.nextLine();
+        storeInMemory(result);
+        //System.out.println("result is "+result);
+        return result;
+
+    }
+    //rotate through options
+    public static void rota(){
+
+        int counter = 0;
+        while (true) {
+            int input = scanner.nextInt();
+
+            if (input == 1) {//1. switch
+                counter++;
+                switch (counter%4){
+                    case 1:
+                        switchDisplay("b");
+                        break;
+                    case 2:
+                        switchDisplay("o");
+                        break;
+                    case 3:
+                        switchDisplay("d");
+                        break;
+                    case 0:
+                        switchDisplay("h");
+                        break;
+                }
+            }
+
+            if (input == 9) {
+                break;
+            }
+        }
+        MainApplication.getMainMenuChoice();
+
+    }
+
+    //Methods to Test
+    //Basic Test
+    public static double add(double n1, double n2){
+        return n1 + n2;
+    }
+
+    public static double square(double num){
+        return num*num;
+    }
+
+    public  static double log(double input){
+        return Math.log10(input);
+    }
+
+    public static double sin(double input){
+        return  Math.sin(input);
+    }
+
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,61 +1,144 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 public class MainApplication {
-
+    // calculator turns on
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        Console.println("0");
+        Console.println("Welcome to our calculator!");
 
-        //Hi Qian
-        // Start loop, us a boolean
+        //Display 0 at start
+        Console.println("\n 0");
+        //Console.println("0");
+        // Start loop, us a boolean while calculator is running
         while (true) {
-            // calculator is active
-            // declare 'choice' as
+            // calculator is active until false
+
+            // Check if memory has a value other than zero
+            double memoryValue = Console.recallMemory();
+            if (memoryValue != 0.0) {
+                double num1 = memoryValue;
+                Console.println("Continue with current value in memory [ " + num1 + " ]");
+            }
+
+            // single character = 'the user choice'
             char choice = getMainMenuChoice();
 
+            // user menu selection cases
             switch (choice) {
-                case 'c':
-                    // Continue with stored value in memory
-                    double num1 = Console.recallMemory();
-                    // Continue with stored value in memory
-                    char operator = Console.getStringInput("Enter operator (+, -, *, /):").charAt(0);
+                case 'b':
+                    double result;
+                    // if memory != 0, then use memory as num1
+                    double num1 = Console.getDoubleInput("Enter first number:");
+
+
+                    //jared
+                    //peter
+                    char operator = Console.getStringInput("Enter operator (+,-,*,/):").charAt(0);;
                     double num2 = Console.getDoubleInput("Enter second number:");
-                    double result = Console.performOperation(num1, operator, num2);
+                    // perform basic operation with operator
+                    result = Console.performOperation(num1, operator, num2);
+                    Console.println("Result: " +  result);
+                    break;
+
+                case 'a':
+                    //
+                    result = Console.AdvFunction();
+                    Console.println("Result: " +  result);
+                    break; // break or return?
+                case 't':
+                    result = Console.TrigFunction();
+                    Console.println("Result: " +  result);
+                    break; // break or return?
+                case 'l':
+                    //
+                    result = Console.Logarithmic();
                     Console.println("Result: " + result);
                     break;
-                case 'y':
-                    // Start a new calculation
-                    num1 = Console.getDoubleInput("Enter first number:");
-                    operator = Console.getStringInput("Enter operator (+, -, *, /):").charAt(0);
-                    num2 = Console.getDoubleInput("Enter second number:");
-                    result = Console.performOperation(num1, operator, num2);
-                    Console.println("Result: " + result);
+                case 'm': // Continue with stored value in memory
+                    // set num1 to memory value
+                    // Continue with stored value in memory
+                    //operator = Console.getStringInput("Enter operator (+, -, *, /):").charAt(0);
+                    //num2 = Console.getDoubleInput("Enter second number:");
+                    result = Console.recallMemory();
+                    Console.println("Starting with memory value: " +  result);
+                    break;
+                case 'c':
+                    // Clear all - Start a new calculation
+                    Console.clearMemory();
+                    break;
+                // Display switch
+                case 's':
+                    Scanner scanner = new Scanner(System.in);
+                    Console.println("switch? binary/h-hexadecimal/o-octal/d-decimal\");");
+                    String s = scanner.nextLine();
+                    //result = Console.switchDisplay(s);
+                    Console.println("Result: " + Console.switchDisplay(s));
+                    break;
+                case 'd':
+                    // Degrees/Radians
+                    Scanner scanner2 = new Scanner(System.in);
+                    Console.println("Switch? r-radians/d-degrees\");");
+                    String s1 = scanner2.nextLine();
+                    //result = Console.switchDisplay(s);
+                    Console.println("Result: " + Console.switchUnitsMode(s1));
+                    break;
+                case 'p':
+                    Console.println("Enter 1 repeatedly to cycle through display.\nEnter 9 to exit cycle.");
+                    Console.rota();
                     break;
                 case 'q':
                     // Quit the calculator
                     Console.println("Final result in memory: " + Console.recallMemory());
                     Console.println("Calculator closed. Goodbye!");
                     return;
+                case 'r':
+                    result = Console.random();
+                    Console.println("Here's a random value! :)   " +  result);
+                    break; // break or return?
+                case 'v':
+                    double absolute = Console.recallMemory();
+                    result = Console.absoluteValue(absolute);
+                    Console.println("Absolute value of memory value is " +  result);
+                    break; // break or return?
+
                 default:
+                    Console.println("Current result in memory : " + Console.recallMemory());
                     Console.println("Invalid choice. Please choose again.");
+
+
             }
         }
     }
 
-    private static char getMainMenuChoice() {
-        Console.println("\nMenu:");
-        Console.println("c. Continue with stored value in memory");
-        Console.println("y. Start a new calculation");
-        Console.println("q. Quit");
-        //Console.println("s. Scientific calculator")
+    // menu // menu // menu // menu // menu // menu // menu // menu // menu
+    public static char getMainMenuChoice() {
+        Console.println("\nChoose (enter character)");
+        Console.println(" b. Basic Math...");
+        Console.println(" a. Advanced Functions..."); //Factorial in here
+        Console.println(" t. Trig Functions..."); //Switch trig units mode (Degrees, Radians)
+        Console.println(" l. Logarithmic Functions...");
+        Console.println(" c. Clear All"); /////!!!!!!!
+        Console.println(" s. Switch...");
+        Console.println(" d. Degree/Radians");
+        Console.println(" r. Custom Function1");
+        Console.println(" v. Custom Function2");
+        Console.println(" p. Cycle through Binary/Hex/Octal/Decimal");
+        Console.println(" m. Memory = " + Console.recallMemory()); //
+        Console.println(" q. Quit");
 
-        String choice = Console.getStringInput("Enter your choice (c/y/q):").toLowerCase();
+        // Choice from input, get string
+        String choice = Console.getStringInput("\nEnter your choice (b/a/t/l/c/s/d/f/m/q):").toLowerCase();
 
-        if (choice.length() == 1 && (choice.equals("c") || choice.equals("y") || choice.equals("q"))) {
+        // if the length is 1, and matches a menu item, get 1st character entered by user
+        if (choice.length() == 1 && (choice.equals("b") || choice.equals("a") || choice.equals("t") || choice.equals("l") || choice.equals("c") || choice.equals("s") || choice.equals("d") || choice.equals("r") || choice.equals("v") || choice.equals("m") || choice.equals("q") || choice.equals("p"))){
             return choice.charAt(0);
         } else {
-            Console.println("Invalid choice. Please choose again.");
+            // if user enters something other than menu commands
+            Console.println("Invalid selection - Please choose again.");
+            // display menu again
             return getMainMenuChoice();
+            //push test
         }
     }
 }
